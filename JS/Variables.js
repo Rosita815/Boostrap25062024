0667a985bff15e0a5bd93e3d350c10c7b64edf8c
Rosita815 <rositajarquin26@gmail.com>
2024-07-02 20:01:06 -0600

    Feat-Variables JavaScript

---
+++ JS/Variables.js
@@ -0,0 +1,18 @@
+/* Lenguaje de programación
+
+JavaScript NO ES LO MISMO QUE Java*/
+
+/* Mostrar mensaje en la consola del navegador */
+console.log("Hola Mundo");
+
+/* 1. Variables var = let, const */
+
+const Nombre = "Rosita";
+const Apellido = "Jarquin";
+let Edad = 27;
+var Direccion= "Puntarenas Centro";
+
+console.log(Nombre);
+console.log(Apellido);
+console.log(Edad);
+console.log(Direccion);
