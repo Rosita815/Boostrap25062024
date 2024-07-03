a3ea3c70cca7186fff3ac563800c272aac1c1aec
Rosita815 <rositajarquin26@gmail.com>
2024-07-02 21:24:12 -0600

    Feat-Tipos datos de JavaScript

---
+++ JS/TipoDeDatos.js
@@ -0,0 +1,25 @@
+/* Lenguaje de programación
+
+JavaScript NO ES LO MISMO QUE Java*/
+
+/* 2. Tipos de datos */
+
+/* 1. Variables var = let, const */
+
+/* Zona para declarar variables */
+let Nombre = "Rosita"; /* Texto, String, Cadena con comillas dobles */
+let Edad = 27; /* Numericos, Integer, Decimal sin comillas dobles */
+let TieneMascotas = True; /* bool, boolean = True o false */
+let TieneGatos = False;
+
+let Frutas = ["Manzana", "Pera", "Uva", true, 35]; /* arreglo, array es una lista de variables [X,Y,Z] permite ingresar varios valores */
+
+/* Objeto{} permite representar datos de la vida real, ejemplo auto, casa, videojuego */
+let Persona = {
+    /* llave: valor */
+    nombre: "Rosita",
+    edad: 27,
+    tieneMascotas: true
+} /* Objeto{} */
+
+/* Fin de Zona para declarar variables */
--- JS/Variables.js
+++ JS/Variables.js
@@ -7,9 +7,22 @@ console.log("Hola Mundo");
 
 /* 1. Variables var = let, const */
 
+/* Zona para declarar variables */
 let Nombre = "Rosita";
 let Apellido = "Jarquin";
 let Edad = 27;
 let Direccion= "Puntarenas Centro";
+let Mensaje = "Hola mi nombre es: "+ Nombre +" y mi apellido es: "+ Apellido +" mi edad es: "+ Edad +" vivo en: "+ Direccion;
+let Mensaje2 = " Es usted mayor de edad?"
+let Ventana;
 
-console.log("Hola mi nombre es : "+ Nombre +", Y mi apellido es "+ Apellido +"mi edad es "+ Edad +" vivo en "+ Direccion);
+
+
+
+
+console.log(Mensaje);
+/* Mostrar mensaje en una ventana emergente */
+alert(Mensaje);
+/* El prompt sirve para capturar el usuario */
+Ventana = prompt(Mensaje2);
+alert(Ventana);
