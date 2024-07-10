62b9176660b0ec2f55cfcea19e7d34adf428027f
Rosita815 <rositajarquin26@gmail.com>
2024-07-09 21:12:07 -0600

    Feat-DOM2

--- JS/MiNombre.js
+++ JS/MiNombre.js
@@ -2,3 +2,12 @@ console.log("Funcionando");
 
 /* Formulario Nombre y Apellido
 Y darle click, Mostrar en un Div */
+
+function Guardar() {
+  const Formulario = document.getElementById("Formulario");
+  const Valor1 = document.getElementById("Valor1").value;
+  const Valor2 = document.getElementById("Valor2").value;
+  const ValorRespuesta = document.getElementById("ValorRespuesta");
+
+  alert(`El valor1 es ${Valor1} el valor 2 es ${Valor2}`)
+}
