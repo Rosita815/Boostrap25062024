3bb5903164868d183006a2f02d5d836254e7a6d4
Rosita815 <rositajarquin26@gmail.com>
2024-07-09 20:20:05 -0600

    Feat-Dom

---
+++ JS/MiNombre.js
@@ -0,0 +1,4 @@
+console.log("Funcionando");
+
+/* Formulario Nombre y Apellido
+Y darle click, Mostrar en un Div */
--- JS/Saludo.js
+++ JS/Saludo.js
@@ -19,13 +19,13 @@ let Mensaje = "Hola Mundo"; */
 /* Seleccionar el elemento del DOM por el ID y cambiar el BG */
 /* document.getElementById("resultado").style.backgroundColor = "blue";  */
 
-/* let DivResultado = document.getElementById("resultado");
+let DivResultado = document.getElementById("resultado");
 
 console.log(DivResultado);
 
 DivResultado.textContent = "Hola Mundo";
 DivResultado.style.backgroundColor = "green";
- */
+
 /* ++++++++++++++++Practica en clase #1+++++++++++++++++++++++++++++++++++++++++++++++++++ */
 let Parrafo = document.getElementById("parrafo");
 
@@ -33,3 +33,12 @@ console.log(Parrafo);
 
 Parrafo.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat non nihil totam deserunt dolor doloribus itaque esse quaerat consequuntur! Consequatur maiores modi autem nesciunt odio, officia animi nostrum architecto voluptate.";
 Parrafo.style.backgroundColor = "green";
+/* Value = lo que hace es obtener el valor actual del DOM*/
+let titulo = document.getElementById("titulo").value;
+/* titulo.textContent = "Gato"; */
+console.log(titulo);
+let NuevoParrafo = document.createElement("p");
+NuevoParrafo.textContent = "Contenido p de la etiqueta";
+/* Para concatenar o unir agregar etiquetas CreateElement al HTML */
+DivResultado.appendChild(NuevoParrafo);
+console.log(NuevoParrafo);
