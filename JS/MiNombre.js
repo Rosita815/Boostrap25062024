c86e1f6cc45f9a7f5965c2a29401be10548ef7a9
Rosita815 <rositajarquin26@gmail.com>
2024-07-09 21:25:44 -0600

    Feat-Calculadora

--- JS/MiNombre.js
+++ JS/MiNombre.js
@@ -5,13 +5,14 @@ Y darle click, Mostrar en un Div */
 
 function Guardar() {
 
-  const Formulario = document.getElementById("Formulario");
-  const Valor1 = document.getElementById("Valor1").value;
-  const Valor2 = document.getElementById("Valor2").value;
-  const ValorRespuesta = document.getElementById("ValorRespuesta");
+    const Formulario = document.getElementById("Formulario");
+    const Valor1 = document.getElementById("Valor1").value;
+    const Valor2 = document.getElementById("Valor2").value;
+    const ValorRespuesta = document.getElementById("ValorRespuesta");
 
-  alert(`El valor1 es ${Valor1} el valor 2 es ${Valor2}`)
+    alert(`El valor1 es ${Valor1} el valor 2 es ${Valor2}`)
 
-  ValorRespuesta.textContent = `El valor1 es ${Valor1} el valor 2 es ${Valor2}`
- debugger
+    ValorRespuesta.textContent = `El valor1 es ${Valor1} el valor 2 es ${Valor2}`
+
+    debugger
 }
