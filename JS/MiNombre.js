780c1ae311704f5966b700182685f94154cc153e
Rosita815 <rositajarquin26@gmail.com>
2024-07-09 21:19:19 -0600

    Feat-DOM_Practica

--- JS/MiNombre.js
+++ JS/MiNombre.js
@@ -4,10 +4,14 @@ console.log("Funcionando");
 Y darle click, Mostrar en un Div */
 
 function Guardar() {
+
   const Formulario = document.getElementById("Formulario");
   const Valor1 = document.getElementById("Valor1").value;
   const Valor2 = document.getElementById("Valor2").value;
   const ValorRespuesta = document.getElementById("ValorRespuesta");
 
   alert(`El valor1 es ${Valor1} el valor 2 es ${Valor2}`)
+
+  ValorRespuesta.textContent = `El valor1 es ${Valor1} el valor 2 es ${Valor2}`
+ debugger
 }
