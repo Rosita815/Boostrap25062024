d9f9164c204543c4f499a2097098e4221613522d
Rosita815 <rositajarquin26@gmail.com>
2024-07-30 21:05:35 -0600

    Feat-LigaDeLaJusticia

--- js/LigaDeLaJusticia.js
+++ js/LigaDeLaJusticia.js
@@ -1,58 +1,69 @@
-console.log("Funciona");
-/* 1--Evento que carga la funcion cuando el DOM este en la pagina */
-/* Control + space -> Para que salgan los eventos*/
-document.addEventListener('DOMContentLoaded',()=>{
-    Mostrar_LigaDeLaJusticia(Heroes) 
+console.log("funcionando")
+// 1--Evento que carga la funcion cuando el DOM este en la pagina 
+//Control + Space
+document.addEventListener('DOMContentLoaded', () => {
+    
+    Mostrar_LigaDeLaJusticia(Heroes) //Carge la pagina  
+
 })
 
-/* 2--Arreglo de objetos */
-const Heroes = [{
-    nombre: "Aquaman",
-    bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
-    img: "img/aquaman.png",
-    casa: "DC"
-  },
-  {
-    nombre: "Batman",
-    bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
-    img: "img/batman.png",
-    casa: "DC"
-  },
-  {
-    nombre: "Superman",
-    bio: "Superman es un superhéroe ficticio que aparece en los cómics publicados por DC Comics. Es uno de los personajes de ficción más populares del siglo XX.",
-    img: "img/superman.png",
-    casa: "DC"
-  },
-  {
-    nombre: "Wonder Woman",
-    bio: "Es una superheroína ficticia creada por el psicólogo y escritor William Moulton Marston para la editorial DC Comics. Sus historias se centran en los ideales del amor, paz y sexualidad.",
-    img: "img/wonder_woman.png",
-    casa: "DC"
-  }]
 
+// 2- Arreglo de objetos
+const Heroes = [
+    {
+      nombre: "Aquaman",
+      bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
+      img: "img/aquaman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Batman",
+      bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
+      img: "img/batman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Superman",
+      bio: "Superman es un superhéroe ficticio que aparece en los cómics publicados por DC Comics. Es uno de los personajes de ficción más populares del siglo XX.",
+      img: "img/superman.png",
+      casa: "DC"
+    },
+    {
+      nombre: "Wonder Woman",
+      bio: "Es una superheroína ficticia creada por el psicólogo y escritor William Moulton Marston para la editorial DC Comics. Sus historias se centran en los ideales del amor, paz y sexualidad.",
+      img: "img/wonder_woman.png",
+      casa: "DC"
+    }
+  ];
 
-/* 3 --Funcion que muestra los heroes
-A. -ID del contenedor
-B. -Plantilla
-C. -Ciclo de la parte 2 (2- Arreglo de Objetos)
-D Meter la Plantilla en el contenedor*/
-function Mostrar_LigaDeLaJusticia(Heroes){ /* Se llama Firma */
-  /*   let Contenedor = document.getElementById('');*/
-  let Plantilla = "";
+// 3-Funcion que muestra los heroes 
+//A  -ID del contendor
+//B  -Plantilla
+//C  -Ciclo de la parte 2 (2- Arreglo de objetos) 
+//D Meter la Plantilla en el contenedor 
+
+function Mostrar_LigaDeLaJusticia(Heroes) { //Firma
+    // let Contenedor //A
+    let Contenedor = document.getElementById("contenedorHeroes")
+    // let Plantilla  //B
+    let plantilla = "";
+    // //C 
     Heroes.forEach(element => {
-        if (element) {
-            Plantilla += `
-            Nombre: ${element.nombre}
-            bio: ${element.bio} `
-        }
-       
+    plantilla += `
+    <div>
+        <h2>${element.nombre}</h2>
+        <p>${element.bio}</p>
+        <img src="${element.img}" alt="${element.nombre}">
+    </div>`
         
     });
-  
-    /* Contenedor.innerHTML = Plantilla */
-    alert(Plantilla)
+
+    // Contenedor.innerHTML = Plantilla //D
+    Contenedor.innerHTML = plantilla
 }
+
+//JSON 
+//String 
 
     
 
