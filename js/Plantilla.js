6b80abb1c478f09c38c7b4807d3265da70ce8734
Rosita815 <rositajarquin26@gmail.com>
2024-07-30 19:43:38 -0600

    Feat-Creacion de estructura palntilla js

---
+++ js/Plantilla.js
@@ -0,0 +1,21 @@
+/* 1--Evento que carga la funcion cuando el DOM este en la pagina */
+/* Control + space -> Para que salgan los eventos*/
+document.addEventListener('DOMContentLoaded',()=>{
+
+})
+
+/* 2--Arreglo de objetos */
+const Heroes 
+
+
+/* 3 --Funcion que muestra los heroes
+A. -ID del contenedor
+B. -Plantilla
+C. -Ciclo de la parte 2 (2- Arreglo de Objetos)
+D Meter la Plantilla en el contenedor*/
+function Mostrar_LigaDeLaJusticia(Heroes){
+    let Contenedor
+    let Plantilla
+
+    Contenedor.innerHTML = Plantilla
+}
